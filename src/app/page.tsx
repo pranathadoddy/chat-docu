22d83cfb6919dfb5d81c97907b86cde34fc96ef6
pranathadoddy <pranathadoddy@gmail.com>
2025-05-03 16:00:29 +0800

    Add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,44 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
+import Link from 'next/link';
+import { LogIn } from 'lucide-react';
 
-export default function Home() {
-  return <h1 className="text-red-500">Hello World</h1>;
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">
+              Chat with your PDF Document
+            </h1>
+            <UserButton />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go To Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers, and professionals using AI
+            to get instant answers and gain deeper insights from research
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>File Upload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
