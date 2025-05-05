b2f62fb649a55559dc1d00f2731dc45e729457f9
pranathadoddy <pranathadoddy@gmail.com>
2025-05-05 16:07:55 +0800

    Add react query and add api to stored the chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
 import { ClerkProvider } from '@clerk/nextjs';
+import Providers from '@/components/Providers';
+import { Toaster } from 'react-hot-toast';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,13 +28,16 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider afterSignOutUrl={'/'}>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
+      <Providers>
+        <html lang="en">
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {children}
+          </body>
+          <Toaster />
+        </html>
+      </Providers>
     </ClerkProvider>
   );
 }
--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,31 +1,75 @@
 'use client';
 import { uploadFileToS3 } from '@/lib/s3';
-import { Inbox } from 'lucide-react';
+import { useMutation } from '@tanstack/react-query';
+import { Inbox, Loader2 } from 'lucide-react';
 import React, { use } from 'react';
+import axios from 'axios';
 import { useDropzone } from 'react-dropzone';
+import toast from 'react-hot-toast';
 
 type Props = {};
 
 const FileUpload = (props: Props) => {
+  const [uploading, setUploading] = React.useState(false);
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post('/api/create-chat', {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: { 'application/pdf': ['.pdf'] },
     maxFiles: 1,
     onDrop: async (acceptedFiles) => {
+      setUploading(true);
       console.log(acceptedFiles);
 
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
-        alert('File size exceeds 10MB limit');
+        toast.error('File size exceeds 10MB limit');
         return;
       }
 
       try {
         const data = await uploadFileToS3(file);
+        if (!data?.file_key || !data.file_name) {
+          toast.error('Something went wrong');
+          return;
+        }
+
+        mutate(
+          {
+            file_key: data.file_key,
+            file_name: data.file_name,
+          },
+          {
+            onSuccess: (data) => {
+              toast.success(data.message);
+            },
+            onError: (error) => {
+              toast.error('Error creating chat. Please try again.');
+              console.error('Error creating chat:', error);
+            },
+          }
+        );
         console.log('File uploaded successfully:', data);
       } catch (error) {
         console.error('Error uploading file:', error);
         alert('Error uploading file. Please try again.');
         return;
+      } finally {
+        setUploading(false);
       }
     },
   });
@@ -36,10 +80,20 @@ const FileUpload = (props: Props) => {
         className="border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col"
       >
         <input {...getInputProps()} />
-        <>
-          <Inbox className="w-10 h-10 text-blue-400" />
-          <p className="text-slate-400">Drag and drop your PDF here</p>
-        </>
+        {uploading || isPending ? (
+          <>
+            {/* loading state */}
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">
+              Uploading your file... Please wait.
+            </p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue-400" />
+            <p className="text-slate-400">Drag and drop your PDF here</p>
+          </>
+        )}
       </div>
     </div>
   );
