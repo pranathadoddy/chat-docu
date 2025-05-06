533cb6eb62673e2a4f32737393aea3d5c9122825
pranathadoddy <pranathadoddy@gmail.com>
2025-05-06 12:08:34 +0800

    Add the persisting message to database

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,12 +1,24 @@
 import { cn } from '@/lib/utils';
 import { Message } from 'ai/react';
+import { Loader2 } from 'lucide-react';
 import React from 'react';
 
 type Props = {
   messages: Message[];
+  isLoading?: boolean;
+  status: 'submitted' | 'streaming' | 'ready' | 'error';
 };
 
-const MessageList = ({ messages }: Props) => {
+const MessageList = ({ messages, isLoading, status }: Props) => {
+  if (isLoading) {
+    return (
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Loader2 className="w-6 h-6 animate-spin" />
+      </div>
+    );
+  }
+
+  console.log('status', status);
   if (!messages) return <></>;
   return (
     <div className="flex flex-col gap-2 px-4">
@@ -32,6 +44,15 @@ const MessageList = ({ messages }: Props) => {
           </div>
         );
       })}
+
+      {status === 'streaming' ||
+        (status === 'submitted' && (
+          <div className="flex justify-start pr-10">
+            <div className="rounded-lg px-3 text-sm py-1 shadow-md ring-1 ring-gray-900/10">
+              <Loader2 className="w-4 h-4 animate-spin" />
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
